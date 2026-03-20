e23c04f24a30e0c2a9713096621b4ed7c1bdcf23
bshriselvamkumar <108592184+bshriselvamkumar@users.noreply.github.com>
2026-03-20 12:54:41 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Website Loaded");
+const slides = document.querySelectorAll('.slide');
+
+window.addEventListener('scroll', () => {
+  slides.forEach(slide => {
+    const top = slide.getBoundingClientRect().top;
+    if (top < window.innerHeight - 50) {
+      slide.classList.add('active');
+    }
+  });
+});
