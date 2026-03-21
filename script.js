191e6f692f789a2afc863b6a36ced42600263a5b
bshriselvamkumar <108592184+bshriselvamkumar@users.noreply.github.com>
2026-03-21 16:19:05 +0530

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-let index = 0;
-const slides = document.querySelectorAll(".slide");
-
-function showSlide() {
-  slides.forEach(slide => slide.classList.remove("active"));
-
-  index++;
-  if (index >= slides.length) index = 0;
-
-  slides[index].classList.add("active");
-}
-
-setInterval(showSlide, 3000);
+console.log("Thendral Clone Ready 🔥");
