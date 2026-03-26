0bd977c4914161a2730e6dcf833bc720b76de4cb
bshriselvamkumar <108592184+bshriselvamkumar@users.noreply.github.com>
2026-03-26 17:34:27 +0530

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.querySelectorAll(".card, .hero-card").forEach(card => {
+// hover animation
+document.querySelectorAll(".card").forEach(card => {
   card.addEventListener("mouseenter", () => {
-    card.style.transform = "scale(1.03)";
-    card.style.transition = "0.3s";
+    card.style.transform = "scale(1.05)";
   });
 
   card.addEventListener("mouseleave", () => {
